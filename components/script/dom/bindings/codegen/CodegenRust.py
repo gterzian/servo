a25972fedb4400ba821bf0738b0f6d5f1f0e7556
Gregory Terzian <gterzian@users.noreply.github.com>
2020-05-03 19:29:17 +0800

    add space in codegen

--- components/script/dom/bindings/codegen/CodegenRust.py
+++ components/script/dom/bindings/codegen/CodegenRust.py
@@ -910,7 +910,7 @@ def getJSToNativeConversionInfo(type, descriptorProvider, failureCode=None,
         assert not isEnforceRange and not isClamp
 
         if failureCode is None:
-            unwrapFailureCode = '''throw_type_error(*cx, "This object is not\
+            unwrapFailureCode = '''throw_type_error(*cx, "This object is not \
                     an instance of ReadableStream.");\n'''
         else:
             unwrapFailureCode = failureCode
