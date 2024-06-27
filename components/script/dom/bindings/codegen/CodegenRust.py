b1d523981a1e86cabc8c2e9e27f09f8b6873b524
Ngo Iok Ui (Wu Yu Wei) <yuweiwu@pm.me>
2024-06-27 18:44:41 +0800

    Add remaining WebIDLs of ReadableStream (#32605)
    * Add Reader's WebIDL files
    
    * Add necessary methods in ReadableStream.webidl
    
    Signed-off-by: gterzian <2792687+gterzian@users.noreply.github.com>

--- components/script/dom/bindings/codegen/CodegenRust.py
+++ components/script/dom/bindings/codegen/CodegenRust.py
@@ -950,7 +950,7 @@ def getJSToNativeConversionInfo(type, descriptorProvider, failureCode=None,
 
         if failureCode is None:
             unwrapFailureCode = '''throw_type_error(*cx, "This object is not \
-                    an instance of ReadableStream.");\n'''
+                    an instance of ReadableStream.");\nreturn false;\n'''
         else:
             unwrapFailureCode = failureCode
 
@@ -958,6 +958,7 @@ def getJSToNativeConversionInfo(type, descriptorProvider, failureCode=None,
             """
             {
                 use crate::realms::{AlreadyInRealm, InRealm};
+                use crate::dom::readablestream::ReadableStream;
                 let in_realm_proof = AlreadyInRealm::assert_for_cx(cx);
                 match ReadableStream::from_js(cx, $${val}.get().to_object(), InRealm::Already(&in_realm_proof)) {
                     Ok(val) => val,
@@ -974,7 +975,7 @@ def getJSToNativeConversionInfo(type, descriptorProvider, failureCode=None,
         templateBody = wrapObjectTemplate(templateBody, "None",
                                           isDefinitelyObject, type, failureCode)
 
-        declType = CGGeneric("DomRoot<ReadableStream>")
+        declType = CGGeneric("DomRoot<dom::readablestream::ReadableStream>")
 
         return handleOptional(templateBody, declType,
                               handleDefault("None"))
