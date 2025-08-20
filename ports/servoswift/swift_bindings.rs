7ffdfdb71935835be272aacc9136745e254b0bb8
gterzian <2792687+gterzian@users.noreply.github.com>
2025-08-20 19:40:02 +0800

    add basic servo init
    Signed-off-by: gterzian <2792687+gterzian@users.noreply.github.com>

--- ports/servoswift/swift_bindings.rs
+++ ports/servoswift/swift_bindings.rs
@@ -8,11 +8,28 @@
 //! All Servo instances and WebViews are managed on the Swift side as opaque pointers.
 
 use std::ffi::{CStr, c_char, c_void};
+use std::rc::Rc;
 
 use url::Url;
 use log::{debug, warn, error};
+use dpi::PhysicalSize;
+use euclid::{Box2D, Size2D, Scale};
+use servo::{Servo, ServoBuilder, WebView, WebViewBuilder};
+use compositing_traits::rendering_context::{RenderingContext, SoftwareRenderingContext};
+use servo::servo_url::ServoUrl;
 
 use crate::init_crypto;
+
+/// Actual Servo data that Swift will hold as opaque pointer
+struct ServoInstance {
+    servo: Servo,
+    rendering_context: Rc<dyn RenderingContext>,
+}
+
+/// Actual WebView data  
+struct WebViewInstance {
+    webview: WebView,
+}
 
 /// Opaque handle that Swift will manage (not used on Rust side)
 pub type ServoHandle = u64;
@@ -68,7 +85,7 @@ pub extern "C" fn create_servo(
     env_logger::init();
     init_crypto();
 
-    let servo_options = if options.is_null() {
+    let _servo_options = if options.is_null() {
         ServoInitOptions::default()
     } else {
         unsafe { *options }
@@ -76,13 +93,35 @@ pub extern "C" fn create_servo(
 
     debug!("Creating Servo instance with size: {}x{}, scale: {}", width, height, scale_factor);
 
-    // For now, return a dummy pointer since we can't easily create a real Servo instance
-    // without proper rendering context integration
-    let dummy_value = Box::new(42u64);
-    let dummy_ptr = Box::into_raw(dummy_value) as *mut c_void;
+    // Create the rendering context
+    let size = PhysicalSize::new(width, height);
+    
+    // Create a software rendering context for now
+    let rendering_context: Rc<dyn RenderingContext> = match SoftwareRenderingContext::new(size) {
+        Ok(ctx) => Rc::new(ctx),
+        Err(e) => {
+            error!("Failed to create rendering context: {:?}", e);
+            return std::ptr::null_mut();
+        }
+    };
 
-    debug!("Created dummy Servo instance at {:p}", dummy_ptr);
-    dummy_ptr
+    // Create Servo builder with the rendering context
+    let builder = ServoBuilder::new(rendering_context.clone());
+    
+    // Set up resource directory if provided
+    // TODO: Use options.resources_dir_path
+    
+    // Create Servo instance
+    let servo = builder.build();
+
+    let instance = ServoInstance {
+        servo,
+        rendering_context,
+    };
+
+    let instance_ptr = Box::into_raw(Box::new(instance)) as *mut c_void;
+    debug!("Created Servo instance at {:p}", instance_ptr);
+    instance_ptr
 }
 
 /// Create a WebView within the Servo instance and return an opaque pointer to it
@@ -90,14 +129,16 @@ pub extern "C" fn create_servo(
 pub extern "C" fn create_webview(
     servo_ptr: *mut c_void,
     url: *const c_char,
-    _width: u32,
-    _height: u32,
-    _scale_factor: f32,
+    width: u32,
+    height: u32,
+    scale_factor: f32,
 ) -> *mut c_void {
     if servo_ptr.is_null() {
         error!("Invalid Servo pointer");
         return std::ptr::null_mut();
     }
+
+    let servo_instance = unsafe { &*(servo_ptr as *const ServoInstance) };
 
     let url_str = if url.is_null() {
         "about:blank"
@@ -113,7 +154,7 @@ pub extern "C" fn create_webview(
         }
     };
 
-    let _parsed_url = match Url::parse(url_str) {
+    let parsed_url = match ServoUrl::parse(url_str) {
         Ok(url) => url,
         Err(e) => {
             error!("Failed to parse URL '{}': {:?}", url_str, e);
@@ -121,12 +162,24 @@ pub extern "C" fn create_webview(
         }
     };
 
-    // For now, return a dummy pointer
-    let dummy_value = Box::new(43u64);
-    let dummy_ptr = Box::into_raw(dummy_value) as *mut c_void;
+    // Create WebView with the specified size
+    let size = PhysicalSize::new(width, height);
+    let rect = Box2D::from_size(Size2D::new(size.width as f32, size.height as f32));
 
-    debug!("Created dummy WebView at {:p} for URL: {}", dummy_ptr, url_str);
-    dummy_ptr
+    let webview = WebViewBuilder::new(&servo_instance.servo)
+        .url(parsed_url.clone().into_url())
+        .hidpi_scale_factor(Scale::new(scale_factor))
+        .build();
+
+    // Resize the WebView to the specified dimensions
+    webview.move_resize(rect);
+
+    // Navigate to the URL
+    webview.load(parsed_url.into_url());    let webview_instance = WebViewInstance { webview };
+    let webview_ptr = Box::into_raw(Box::new(webview_instance)) as *mut c_void;
+
+    debug!("Created WebView at {:p} for URL: {}", webview_ptr, url_str);
+    webview_ptr
 }
 
 /// Load a URL in an existing WebView
@@ -151,12 +204,15 @@ pub extern "C" fn webview_load_url(
         }
     };
 
-    let _parsed_url = match Url::parse(url_str) {
+    let parsed_url = match Url::parse(url_str) {
         Ok(url) => url,
         Err(_) => return ServoError::InvalidUrl,
     };
 
-    debug!("Load URL {} in WebView {:p}", url_str, webview_ptr);
+    let webview_instance = unsafe { &*(webview_ptr as *mut WebViewInstance) };
+    webview_instance.webview.load(parsed_url);
+
+    debug!("Loaded URL {} in WebView {:p}", url_str, webview_ptr);
     ServoError::Success
 }
 
@@ -212,7 +268,8 @@ pub extern "C" fn destroy_webview(
     }
 
     unsafe {
-        let _dummy = Box::from_raw(webview_ptr as *mut u64);
+        let _webview_instance = Box::from_raw(webview_ptr as *mut WebViewInstance);
+        // WebView will be dropped automatically
     }
 
     debug!("Destroyed WebView at {:p}", webview_ptr);
@@ -227,7 +284,8 @@ pub extern "C" fn destroy_servo(servo_ptr: *mut c_void) -> ServoError {
     }
 
     unsafe {
-        let _dummy = Box::from_raw(servo_ptr as *mut u64);
+        let _servo_instance = Box::from_raw(servo_ptr as *mut ServoInstance);
+        // Servo instance will be dropped automatically
     }
 
     debug!("Destroyed Servo instance at {:p}", servo_ptr);
@@ -239,4 +297,35 @@ pub extern "C" fn destroy_servo(servo_ptr: *mut c_void) -> ServoError {
 pub extern "C" fn version() -> *const c_char {
     static VERSION: &str = concat!(env!("CARGO_PKG_VERSION"));
     VERSION.as_ptr() as *const c_char
+}
+
+/// Get a render callback function that can blit the Servo framebuffer to a parent OpenGL context
+/// Returns a function pointer that takes (gl_context, x, y, width, height) parameters
+#[unsafe(no_mangle)]
+pub extern "C" fn get_render_callback(
+    servo_ptr: *mut c_void,
+) -> Option<extern "C" fn(*const c_void, i32, i32, i32, i32)> {
+    if servo_ptr.is_null() {
+        error!("Invalid Servo pointer");
+        return None;
+    }
+
+    // For now, return a simple dummy callback
+    // TODO: Implement actual render_to_parent_callback integration
+    debug!("Render callback requested for Servo {:p}", servo_ptr);
+    Some(render_to_parent_static)
+}
+
+/// Static function that handles rendering to parent context
+/// This will be called from Swift with OpenGL context and coordinates
+extern "C" fn render_to_parent_static(
+    _gl_context: *const c_void,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+) {
+    debug!("Render to parent called: ({}, {}) {}x{}", x, y, width, height);
+    // TODO: Implement actual blit operation
+    // This would need access to the specific servo instance and its render callback
 }
