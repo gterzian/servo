d98391774390de683a312863d147d0710bef3068
gterzian <2792687+gterzian@users.noreply.github.com>
2026-02-19 16:56:02 +0700

    add basic backend
    Signed-off-by: gterzian <2792687+gterzian@users.noreply.github.com>

--- components/script/dom/debuggerglobalscope.rs
+++ components/script/dom/debuggerglobalscope.rs
@@ -28,6 +28,7 @@ use script_bindings::reflector::DomObject;
 use script_bindings::str::DOMString;
 use servo_url::{ImmutableOrigin, MutableOrigin, ServoUrl};
 use storage_traits::StorageThreads;
+use webnn_traits::WebNNMsg;
 
 use crate::dom::bindings::codegen::Bindings::DebuggerGlobalScopeBinding;
 use crate::dom::bindings::codegen::Bindings::DebuggerInterruptEventBinding::{
@@ -87,6 +88,7 @@ impl DebuggerGlobalScope {
         script_to_embedder_chan: ScriptToEmbedderChan,
         resource_threads: ResourceThreads,
         storage_threads: StorageThreads,
+        webnn_sender: base::generic_channel::GenericSender<WebNNMsg>,
         #[cfg(feature = "webgpu")] gpu_id_hub: std::sync::Arc<IdentityHub>,
         cx: &mut JSContext,
     ) -> DomRoot<Self> {
@@ -100,6 +102,7 @@ impl DebuggerGlobalScope {
                 script_to_embedder_chan,
                 resource_threads,
                 storage_threads,
+                webnn_sender,
                 MutableOrigin::new(ImmutableOrigin::new_opaque()),
                 ServoUrl::parse_with_base(None, "about:internal/debugger")
                     .expect("Guaranteed by argument"),
